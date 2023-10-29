da9e8f26b84c1f4d67b903dcd96d27459b2948c8
Dave Held <dav3h3ld@gmail.com>
2023-10-29 14:03:01 -0700

    fix - linting - unused import

--- client/src/components/icons/Trash.tsx
+++ client/src/components/icons/Trash.tsx
@@ -1,4 +1,3 @@
-import type { MouseEvent } from "react";
 import type { ButtonBaseProps } from "../button";
 import { ButtonBase } from "../button/ButtonBase";
 import { ScalableSVG } from "./index.d";
